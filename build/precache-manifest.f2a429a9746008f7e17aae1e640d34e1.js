8a5d464945a3a32fd4aaa6fbcaba7aae64e0faee
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-04-19 19:15:29 -0400

    first build

---
+++ build/precache-manifest.f2a429a9746008f7e17aae1e640d34e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0540b9e123a710b046e6",
+    "url": "/static/js/main.f6ca3ab1.chunk.js"
+  },
+  {
+    "revision": "eb980e2eaf4a5d60c1d7",
+    "url": "/static/js/2.525590c0.chunk.js"
+  },
+  {
+    "revision": "0540b9e123a710b046e6",
+    "url": "/static/css/main.f5a5df84.chunk.css"
+  },
+  {
+    "revision": "12daa78d8203efddcd64a619d9925bb0",
+    "url": "/index.html"
+  }
+];
